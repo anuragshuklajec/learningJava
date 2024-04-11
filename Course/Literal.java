36087a411ed616c7ee5bcbefee0deca1ece778d1
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-12 00:12:12 +0530

    Literal

---
+++ Course/Literal.java
@@ -0,0 +1,20 @@
+public class Literal {
+    public static void main(String[] args) {
+        // literals
+        // binary numbers
+        int num1 = 0b101;
+        System.out.println(num1);
+        // hexadecimal
+        int num2 = 0x7E;
+        System.out.println(num2);
+        // you can even use _ between numbers for yout convience
+        int num3 = 10__00__00_000;
+        System.out.println(num3);
+        // e representation for large numbers
+        double num4 = 12e10;
+        System.out.println(num4);
+        
+
+
+    }
+}
