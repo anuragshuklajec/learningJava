83434d057df5f1383900de704bff744a842a59f5
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-12 00:04:17 +0530

    data-types

---
+++ Course/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Integer
+        int num1 = 44;
+        System.out.println(num1);
+
+        // Byte - -127 to 127
+        byte by = 127;
+        System.out.println(by);
+
+        // short
+        short sh = 558;
+        System.out.println(sh);
+
+        // long
+        long l = 500l;
+        System.out.println(l);
+
+        // Float
+        float floaty = 6.5f;
+        System.out.println(floaty);
+
+        // double - default in Java
+        double doubly = 5.6;
+        System.out.println(doubly);
+
+        // character
+        char c = 'k';
+        System.out.println(c);
+
+        // Boolean - doesn't work with 0 and 1 in Java
+        boolean bool = true;
+        System.out.println(bool);
+
+        
+    }
+}
