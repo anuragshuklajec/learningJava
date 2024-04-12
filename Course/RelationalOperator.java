838111b4eae8d19aa735b94a6d95979fdd864ed6
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-12 12:47:04 +0530

    RelationalOperator

---
+++ Course/RelationalOperator.java
@@ -0,0 +1,30 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+        // less than
+        boolean result1 = x < y;
+        System.out.println(result1);
+
+        // greater than
+        boolean result2 = x > y;
+        System.out.println(result2);
+
+        // equal to 
+        boolean result3 = x == y;
+        System.out.println(result3);
+
+        // not equal to
+        boolean result4 = x != y;
+        System.out.println(result4);
+
+        // greater than equal to 
+        boolean result5 = x >= y;
+        System.out.println(result5);
+
+        // less than equal to
+        boolean result6 = x <= y;
+        System.out.println(result6);
+    }
+}
