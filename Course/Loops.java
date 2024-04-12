50cefef2738b0e7d16d4796dfd5879e88f0a0f48
anuragshuklajec <anuragshuklajec@gmail.com>
2024-04-12 19:12:04 +0530

    Loops

---
+++ Course/Loops.java
@@ -0,0 +1,25 @@
+/* eslint-disable */
+public class Loops {
+    public static void main(String[] args) {
+        // While loop
+        int i = 0;
+        while(i < 4){
+            System.out.println(i);
+            i++;
+        }
+
+        // Do while loop
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 4);
+
+        // For loop
+        for(int k = 0; k < 4; k++){
+            System.out.println(k);
+        }
+
+    }
+    
+}
